dc2cbe0c258f9c6b0d5d69a455c5044c4fd57890
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-02-18 04:41:08 +0000

    17章

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about',as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
