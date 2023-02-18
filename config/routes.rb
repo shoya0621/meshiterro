aba34315a57686a99985c199fb84ca3411cc2c76
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-02-18 15:16:46 +0000

    19章

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
